10946fa3fa7b837675fbe4ab14bd082ea7045959
Raka Rasell <bp004771@ap.mds.honda.com>
2024-10-03 18:33:58 +0700

    up.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div data-theme="retro" className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      < main className="flex flex-col gap-8 row-start-2 items-center sm:items-start" >
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <button className="btn btn-primary">asdasd</button>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -48,7 +49,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
+      </main >
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -96,6 +97,6 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+    </div >
   );
 }
